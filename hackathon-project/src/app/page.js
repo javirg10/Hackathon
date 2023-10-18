ecabe951a7a7c2fbf4829b17f380bc5396de043b
JWsteel <janwillemcurinckx@gmail.com>
2023-10-18 14:41:15 +0200

    Added basic shit

--- hackathon-project/src/app/page.js
+++ hackathon-project/src/app/page.js
@@ -1,7 +1,24 @@
+'use client';
 import Image from 'next/image'
 
+
 export default function Home() {
+
+  function handleClick(){
+    alert("clicked");
+    console.log("clicked");
+  }
+
   return (
+    <main>
+      <h1>Quiz to recognize AI Images (temp)</h1>
+      <div id="quizContent">
+        <p>insert Quiz content here</p>
+
+        <button onClick={handleClick}>Submit</button>
+      </div>
+    </main>
+    /*
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -109,5 +126,6 @@ export default function Home() {
         </a>
       </div>
     </main>
+    */
   )
 }
